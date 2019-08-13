53b7f83760ef19dac4563ff687c0c7f352679762
Vincent Pace <vpace@nhpace.net>
2019-08-12 21:14:50 -0400

    Add onTextChange and onClick handlers for NewItem component

--- src/components/NewItemForm/NewItem.js
+++ src/components/NewItemForm/NewItem.js
@@ -2,15 +2,36 @@ import React from 'react';
 import './NewItem.css';
 
 class NewItem extends React.Component {
+    state = {
+        text: null
+    };
+
+    addBtnHandler = () => {
+        /*{Add new label with this.state.text}*/
+    };
+
+    textChangeHandler = () => {
+        return (
+            this.setState({
+                text: 'x'
+            }, () => {
+                console.log(this.state.text)
+            })
+        )
+    };
 
     render() {
         return (
             <div className='new-item-input'>
                 <span>
                     <label>New Item</label>
-                    <input />
+                    <input onChange={this.textChangeHandler}/>
                 </span>
-                <button className='btn-add'>Add</button>
+                <button
+                    className='btn-add'
+                    onClick={this.addBtnHandler}>
+                    Add
+                </button>
             </div>
         );
     }
